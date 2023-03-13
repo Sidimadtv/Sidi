0fcbf704dacf47cb5819635cb174d1a115f70b5a
SidimaD <43736678+Sidimadtv@users.noreply.github.com>
2023-03-13 18:53:57 +0100

    Add files via upload

---
+++ js/q.js
@@ -0,0 +1,349 @@
+var stages = {
+    stage1: {
+        data: [
+            {
+              "question": "في أي يوم خُلق سيدنا آدم عليه السلام؟",
+              "options": [
+                "A: يوم السبت",
+                "B: يوم الإثنين",
+                "C: يوم الجمعة",
+                "D: يوم الأحد",
+              ],
+              "ans": "C"
+            },
+            {
+              "question": "ما هي المدة التي نام فيها أهل الكهف؟",
+              "options": [
+                "A: 309 سنة",
+                "B: 308 سنة",
+                "C: 310 سنة",
+                "D: 311 سنة",
+             ],
+             "ans": "A"
+            },
+            {
+              "question": "من هو النبي الذي آمن به جميع قومه؟",
+              "options": [
+                "A: نوح عليه السلام",
+                "B: شعيب عليه السلام",
+                "C: إبراهيم عليه السلام",
+                "D: يونس عليه السلام",
+              ],
+              "ans": "D"
+            },
+            {
+              "question": "كم مرة ذُكر اسم الرسول محمد صلى الله عليه وسلم في القرآن؟",
+              "options": [
+                "A: 4 مرات",
+                "B: 5 مرات",
+                "C: 6 مرات",
+                "D: 7 مرات",
+              ],
+              "ans": "A"
+            },
+            {
+              "question": "كم عدد السجدات في القرآن الكريم؟",
+              "options": [
+                "A: 14 سجدة",
+                "B: 15 سجدة",
+                "C: 17 سجدة",
+                "D: 18 سجدة",
+              ],
+              "ans": "B"
+            },
+            {
+             "question": "ماذا يقصد بالشرك الأصغر؟",
+              "options": [
+                "A: الرياء",
+                "B: الكذب",
+                "C: النفاق",
+                "D: الخيانة",
+              ],
+              "ans": "A"
+            },
+            {
+             "question": "من هو النبي الذي لقب بالصديق؟",
+              "options": [
+                "A: يونس عليه السلام",
+                "B: يوسف عليه السلام",
+                "C: يعقوب عليه السلام",
+                "D: ابراهيم عليه السلام",
+              ],
+              "ans": "B"
+            },
+            {
+             "question": "كم عدد الأشهر الحرم؟",
+              "options": [
+                "A: شهران",
+                "B: ثلاثة أشهر",
+                "C: أربعة أشهر",
+                "D: خمسة أشهر",
+              ],
+              "ans": "C"
+            },
+            {
+              "question": "متى فتحت مكة المكرمة؟",
+              "options": [
+                "A: 4 هـ",
+                "B: 6 هـ",
+                "C: 8 هـ",
+                "D: 7 هـ",
+              ],
+              "ans": "C"
+            },
+            {
+              "question": "اسم أول صحابي قرأ القرآن جهرة؟",
+              "options": [
+                "A: عبد الله بن مسعود",
+                "B: عبد الله بن عباس",
+                "C: عبد الله بن عمرو بن العاص",
+                "D: عبد الله بن الزبير",
+              ],
+              "ans": "A"
+            },
+            {
+              "question": "من هي أخر زوجات النبي التي توفيت؟",
+              "options": [
+                "A: السيدة زينب بنت جحش",
+                "B: السيدة عائشة بنت أبي بكر",
+                "C: السيدة أم سلمة",
+                "D: السيدة سودة بنت زمعة",
+              ],
+              "ans": "C"
+            },
+            {
+              "question": "سورة قصيرة من قرأها ثلاثا فكأنما قرأ كل القرآن؟",
+              "options": [
+                "A: سورة الناس",
+                "B: سورة الفلق",
+                "C: سورة الإخلاص",
+                "D: سورة الفاتحة",
+              ],
+              "ans": "C"
+            },
+            {
+              "question": "كم مرة حج محمد رسول الله عليه الصلاة والسلام؟",
+              "options": [
+                "A: مرة واحدة",
+                "B: مرتين",
+                "C: ثلاث مرات",
+                "D: أربع مرات",
+             ],
+             "ans": "A"
+            },
+            {
+              "question": "ما هي غزوة الفرقان؟",
+              "options": [
+                "A: غزوة بدر",
+                "B: غزوة أحد",
+                "C: غزوة الخندق",
+                "D: غزوة الطائف",
+              ],
+              "ans": "A"
+            },
+            {
+              "question": "ما هي السورة التي تسمى بقلب القرآن؟",
+              "options": [
+                "A: سورة الفاتحة",
+                "B: سورة البقرة",
+                "C: سورة يس",
+                "D: سورة الرحمن",
+              ],
+              "ans": "C"
+            },
+            {
+              "question": "في أي عام تم فرض فريضة الصيام؟",
+              "options": [
+                "A: العام الأول من الهجرة",
+                "B: العام الثاني من الهجرة",
+                "C: العام الثالث من الهجرة",
+                "D: العام الرابع من الهجرة",
+              ],
+              "ans": "B"
+            },
+            {
+              "question": "كم كان عمر النبي محمد صلى الله عليه وسلم عندما توفيت أمه؟",
+              "options": [
+                "A: 3 سنوات",
+                "B: 4 سنوات",
+                "C: 5 سنوات",
+                "D: 6 سنوات",
+              ],
+              "ans": "C"
+            },
+           
+          
+           {
+              "question": "الشهر الثالث في التقويم الهجري؟",
+              "options": [
+                "A: شعبان",
+                "B: جمادى الآخرة",
+                "C: ذو القعدة",
+                "D: ربيع الأول",
+              ],
+              "ans": "D"
+            },
+          
+          
+          
+          {
+              "question": "ما هي أطول سورة في القرآن الكريم؟",
+              "options": [
+                "A: سورة البقرة",
+                "B: سورة آل عمران",
+                "C: سورة النساء",
+                "D: سورة المائدة",
+              ],
+              "ans": "A"
+            }
+        ]
+    },
+    stage2: {
+      data: [
+          {
+            "question": "What is the full meaning of PHP?",
+            "options": [
+              "A: Hypertext Preprocessor",
+              "B: Pre Hypertext Processor",
+              "C: Page High Power",
+              "D: Power High Page",
+            ],
+            "ans": "A"
+          },
+          {
+            "question": "In JavaScript which is an abstract data type?",
+            "options": [
+              "A: Array",
+              "B: List",
+              "C: Symbol",
+              "D: String",
+           ],
+           "ans": "B"
+          },
+          {
+            "question": "Which is a PHP framework?",
+            "options": [
+              "A: Composer",
+              "B: WebGl",
+              "C: Mongoose",
+              "D: Laravel",
+            ],
+            "ans": "D"
+          },
+          {
+            "question": "Which is the largest country by population?",
+            "options": [
+              "A: Russia",
+              "B: United States of America",
+              "C: China",
+              "D: Japan",
+            ],
+            "ans": "C"
+          },
+          {
+            "question": "Flourine element is known as?",
+            "options": [
+              "A: Halogen",
+              "B: Ester",
+              "C: Rare gas",
+              "D: Ethanol",
+            ],
+            "ans": "A"
+          },
+          {
+            "question": "Which of these is not a framework for building Android application?",
+            "options": [
+              "A: React Native",
+              "B: Ionic",
+              "C: Phonegap",
+              "D: Kotlin",
+            ],
+            "ans": "D"
+          },
+          {
+            "question": "Which of these is not a metal?",
+            "options": [
+              "A: Calcium",
+              "B: Aluminium",
+              "C: Argon",
+              "D: Sodium",
+            ],
+            "ans": "C"
+          }
+      ]
+  },
+  stage3: {
+    data: [
+        {
+          "question": "Chai is a tool used for?",
+          "options": [
+            "A: Testing",
+            "B: Module bundling",
+            "C: Designing",
+            "D: Data Science",
+          ],
+          "ans": "A"
+        },
+        {
+          "question": "Which is not used for module bundling and task running?",
+          "options": [
+            "A: Webpack",
+            "B: Mocha",
+            "C: Gulp",
+            "D: Grunt",
+         ],
+         "ans": "B"
+        },
+        {
+          "question": "Tkinter is a python library used for?",
+          "options": [
+            "A: Artificial Intelligence",
+            "B: Machine Learning",
+            "C: Desktop development",
+            "D: Mobile development",
+          ],
+          "ans": "C"
+        },
+        {
+          "question": "Golang was created at?",
+          "options": [
+            "A: Facebook",
+            "B: Zeit",
+            "C: Microsoft",
+            "D: Google",
+          ],
+          "ans": "D"
+        },
+        {
+          "question": "Scrapy is a framework used for?",
+          "options": [
+            "A: Web crawling",
+            "B: Game development",
+            "C: Network sniffing",
+            "D: Web development",
+          ],
+          "ans": "A"
+        },
+        {
+          "question": "Nodejs is usually known for its?",
+          "options": [
+            "A: Speed",
+            "B: Efficiency",
+            "C: Simplicity",
+            "D: Asyncronousity",
+          ],
+          "ans": "D"
+        },
+        {
+          "question": "The largest planet is?",
+          "options": [
+            "A: Saturn",
+            "B: Jupiter",
+            "C: Earth",
+            "D: Uranium",
+          ],
+          "ans": "B"
+        }
+    ]
+  }
+}    
